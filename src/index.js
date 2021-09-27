c4fab4d71c431ed01c99be53ddd7647afd049a99
magaton <milan.agatonovic@gmail.com>
2021-09-27 17:45:16 +0200

    added more query examples. disabled schema validation added WHERE and OPTIONS in both custom query types ORDER BY and LIMIT resolved in cypher

--- src/index.js
+++ src/index.js
@@ -30,7 +30,14 @@ const driver = neo4j.driver(
  * https://neo4j.com/docs/graphql-manual/current/
  */
 
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver })
+//const neoSchema = new Neo4jGraphQL({ typeDefs, driver })
+const neoSchema = new Neo4jGraphQL({
+  typeDefs,
+  driver,
+  config: {
+      skipValidateTypeDefs: true,
+  },
+})
 
 
 async function startApolloServer(){
